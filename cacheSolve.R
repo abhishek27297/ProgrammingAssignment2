445b660ac61e623b62329bb2ea8720748b859005
Abhishek Singh <abhishek27297@gmail.com>
2017-06-06 20:31:26 +0530

    Update cacheSolve.R

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,1 +1,13 @@
-
+#computes inverse with help of makeCacheMatrix
+cacheSolve<-function(x,...){
+  i <- x$getinverse()    #get the inverse
+  if(!is.null(i)) {     #check if it already exsists in cache
+    message("getting cached data")
+    return(i)           #return that cached value
+  }
+  mat <- x$get()    #if not solve for inverse and set it
+  i <- solve(mat)
+  x$setinverse(i)
+  i
+  
+}
