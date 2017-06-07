64886360e01b225980ac30d99efad8d9084fcb6f
Abhishek Singh <abhishek27297@gmail.com>
2017-06-07 17:44:24 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,19 @@ makeCacheMatrix<-function(x=matrix()){
        getinverse = getinverse)
   
 }
+
+
+
+#computes inverse with help of makeCacheMatrix
+cacheSolve<-function(x,...){
+  i <- x$getinverse()    #get the inverse
+  if(!is.null(i)) {     #check if it already exsists in cache
+    message("getting cached data")
+    return(i)           #return that cached value
+  }
+  mat <- x$get()    #if not solve for inverse and set it
+  i <- solve(mat)
+  x$setinverse(i)
+  i
+  
+}
